d14a3b25fc7ddb55c156e19c1ade71715ca2fc43
Harald Greve <hgreve@macmillan.org.uk>
2015-11-06 11:14:16 +0000

    Added JSON result data

--- ImgLatLong/ImgLatLong/Controllers/ImagesController.cs
+++ ImgLatLong/ImgLatLong/Controllers/ImagesController.cs
@@ -18,6 +18,57 @@ namespace ImgLatLong.Controllers
         string _Instagram_client_id = "833d878fa43c4f24b80a4631e50bfb1a";
         private const string AccessToken = "2233692468.97d3404.d6f0d30225504e978d424b8db1091997";
         private double plusminusLatLng = 0.01;
+
+        private List<PinModel> _events = new List<PinModel>();    
+        
+        
+       
+        private IEnumerable<PinModel> FetchPinData()
+        {
+            _events.Add(new PinModel
+            {
+                EventName = "Vauxhall Hack Day 2015",
+                Latitude = 51.4886,
+                Longitude = -0.1207,
+                LocationName = "89 Albert Embankment, London"
+            });
+
+            _events.Add(new PinModel
+            {
+                EventName = "Hackney Empire Charity Concert",
+                Latitude = 51.5333,
+                Longitude = 0.0833,
+                LocationName = "Hackney, London"
+            });
+
+            _events.Add(new PinModel
+            {
+                EventName = "Wimbledon Tennis Tournament",
+                Latitude = 51.4235,
+                Longitude = 0.2171,
+                LocationName = "Wimbledon"
+            });
+
+            _events.Add(new PinModel
+            {
+                EventName = "Hyde Park Softball Tournament",
+                Latitude = 51.5086,
+                Longitude = 0.1636,
+                LocationName = "Hyde Park, London"
+            });
+
+            _events.Add(new PinModel
+            {
+                EventName = "Wembley Charity Football Match",
+                Latitude = 51.5560,
+                Longitude = 0.3042,
+                LocationName = "Wembley Stadium"
+            });
+
+            return _events;
+
+        }
+
         // GET: Images
         public ActionResult Index()
         {
@@ -46,46 +97,8 @@ namespace ImgLatLong.Controllers
                 var coordinates = GetLatLngFromLocation(query);
 
 
-
-                string uriInstagram =
-                    string.Format("https://api.instagram.com/v1/tags/teammacmillan/media/recent?access_token={0}",
-                        AccessToken);
-
-                var result = new WebClient().DownloadString(uriInstagram);
-
-                JavaScriptSerializer jss = new JavaScriptSerializer();
-
-                var instaJson = jss.Deserialize<dynamic>(result);
-
-                var data = instaJson["data"];
-
-                List<ImageModel> imgList = new List<ImageModel>();
-
-                foreach (var img in data)
-                {
-                    if (img["location"] != null)
-                    {
-
-                        var lat = (double)img["location"]["latitude"];
-                        var lng = (double)img["location"]["longitude"];
-
-                        if (lat + plusminusLatLng > coordinates[0]
-                            && lat - plusminusLatLng < coordinates[0]
-                            && lng + plusminusLatLng > coordinates[1]
-                            && lng - plusminusLatLng < coordinates[1])
-                        {
-                            var name = img["location"]["name"];
-
-                            imgList.Add(new ImageModel
-                            {
-                                ImageUrl = img["images"]["low_resolution"]["url"],
-                                Latitude = lat,
-                                Longitude = lng,
-                                Name = name
-                            });
-                        }
-                    }
-                }
+                var imgList = GetImages(coordinates[0], coordinates[1]);
+                
 
                 return new ImageListViewModel { ImageList = imgList };
             }
@@ -105,7 +118,61 @@ namespace ImgLatLong.Controllers
 
         public ActionResult GetImageJsonForLatLng(string lat, string lng)
         {
-            return null;
+            ImageListViewModel model;
+
+            var imgList = GetImages(Convert.ToDouble(lat), Convert.ToDouble(lng));
+
+            return Json(new ImageListViewModel { ImageList = imgList }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetEventDataJson()
+        {
+            return Json(FetchPinData(), JsonRequestBehavior.AllowGet);
+        }
+
+        private IEnumerable<ImageModel> GetImages(double lati, double lngi)
+        {
+            string uriInstagram =
+                    string.Format("https://api.instagram.com/v1/tags/teammacmillan/media/recent?access_token={0}",
+                        AccessToken);
+
+            var result = new WebClient().DownloadString(uriInstagram);
+
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+
+            var instaJson = jss.Deserialize<dynamic>(result);
+
+            var data = instaJson["data"];
+
+            List<ImageModel> imgList = new List<ImageModel>();
+
+            foreach (var img in data)
+            {
+                if (img["location"] != null)
+                {
+
+                    var lat = (double)img["location"]["latitude"];
+                    var lng = (double)img["location"]["longitude"];
+
+                    if (lat + plusminusLatLng > lati
+                        && lat - plusminusLatLng < lati
+                        && lng + plusminusLatLng > lngi
+                        && lng - plusminusLatLng < lngi)
+                    {
+                        var name = img["location"]["name"];
+
+                        imgList.Add(new ImageModel
+                        {
+                            ImageUrl = img["images"]["low_resolution"]["url"],
+                            Latitude = lat,
+                            Longitude = lng,
+                            Name = name
+                        });
+                    }
+                }
+            }
+
+            return imgList;
         }
 
         private List<double> GetLatLngFromLocation(string query)
---
+++ ImgLatLong/ImgLatLong/Models/PinModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImgLatLong.Models
+{
+    public class PinModel
+    {
+        public string EventName { get; set; }
+
+        public string LocationName { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
